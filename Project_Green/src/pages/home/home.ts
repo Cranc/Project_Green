17f95abea08f245e085a75e4df6b810a488cfcfc
Dennis Lindner <lindner_dennis@yahoo.de>
2018-01-22 15:36:31 +0100

    Added basic databaseservice and example for user display

--- Project_Green/src/pages/home/home.ts
+++ Project_Green/src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DatabaseServiceProvider } from '../../providers/database-service/database-service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { debug } from 'util';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  accounts : FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public db:DatabaseServiceProvider) {
+    this.accounts = this.db.listAccounts();
+    debugger;
   }
 
 }
--- Project_Green/src/providers/database-service/database-service.ts
+++ Project_Green/src/providers/database-service/database-service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { database } from 'firebase/app';
+import { AngularFireModule } from 'angularfire2';
 
 /*
   Generated class for the DatabaseServiceProvider provider.
@@ -11,9 +14,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 @Injectable()
 export class DatabaseServiceProvider {
 
-  constructor(afa: AngularFireAuth) {
-    afa.auth.signInWithPopup;
+  constructor(private _af: AngularFireAuth, private _db: AngularFireDatabase) {
+    //af.auth.signInWithPopup;
+    _af.authState;
     console.log('Hello DatabaseServiceProvider Provider');
+  }
+
+  public listAccounts(): FirebaseListObservable<any[]>{
+    return this._db.list('/users');
   }
 
 }

6df5638433cb88ecc2a7de97c89463de0bbc1027
Dennis Lindner <lindner_dennis@yahoo.de>
2018-01-23 15:03:14 +0100

    Added Github Sign In Method

--- Project_Green/src/pages/home/home.ts
+++ Project_Green/src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DatabaseServiceProvider } from '../../providers/database-service/database-service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
@@ -13,11 +15,20 @@ export class HomePage {
   plantgroup : FirebaseListObservable<any[]>;
   plantsubgroup : FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, public db:DatabaseServiceProvider) {
+  constructor(public navCtrl: NavController, public db:DatabaseServiceProvider, public auth:AngularFireAuth) {
     this.accounts = this.db.listAccounts();
     this.blossomcolor = this.db.listBlossomColor();
     this.plantgroup = this.db.listPlantGroup();
     this.plantsubgroup = this.db.listPlantSubGroup();
   }
 
+  public signInWithGitHub() {
+    this.auth.auth
+      .signInWithPopup(new firebase.auth.GithubAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  public signOut() {
+    this.auth.auth.signOut();
+  }
 }
